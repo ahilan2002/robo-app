a0d42fc3d9929f21d6dcfaecebaab63d2d91e1e9
ahilan2002 <ahilanarivarasu5@gmail.com>
2022-06-29 20:41:30 +0530

    Updates

---
+++ precache-manifest.a8579fd834b5bd69ea5a8c31b8a05e7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fe1cc7e3d0a67bfd93e",
+    "url": "/robo-app/static/css/main.fe9e67d7.chunk.css"
+  },
+  {
+    "revision": "2fe1cc7e3d0a67bfd93e",
+    "url": "/robo-app/static/js/main.2fe1cc7e.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/robo-app/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "738c96938634f36503f2",
+    "url": "/robo-app/static/css/2.0068c751.chunk.css"
+  },
+  {
+    "revision": "738c96938634f36503f2",
+    "url": "/robo-app/static/js/2.738c9693.chunk.js"
+  },
+  {
+    "revision": "20060304fd130bcbfa56",
+    "url": "/robo-app/static/js/runtime~main.20060304.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a1903d95a83420a8a2cc49caed05a5fa",
+    "url": "/robo-app/index.html"
+  }
+];
